97d1200570d9ae15b3ccb00c0b5a9b8be8abebf9
GeorginaAchieng <georginaodhiambo@yahoo.com>
2024-05-01 17:42:47 +0300

    TBScreeningTool

---
+++ app/src/main/java/com/example/tbscreeningtoolapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tbscreeningtoolapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tbscreeningtoolapplication.app.TBScreening
+import com.example.tbscreeningtoolapplication.ui.theme.TBScreeningToolApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TBScreening()
+
+
+                }
+            }
+        }
+
+
